01aed795de7703c9aa11e073f2b34dcf4f928e66
fajarrahmaat27 <fajarrahmat934@gmail.com>
2023-04-11 17:19:00 +0700

    menambahkan prototype.html

---
+++ script/script.js
@@ -0,0 +1,15 @@
+// const header = document.getElementsByClassName("header")[0]
+
+// function handleHeader() {
+//     window.addEventListener("scroll", () => {
+//         const scrolling = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+
+//         if(scrolling > 10) {
+//             header.style.top = "-80px"
+//         } else {
+//             header.style.top = "0"
+//         }
+//     })
+// }
+
+// handleHeader()
