043f0b04f1d90230af74e877e6dd2e92246838a0
fajarrahmaat27 <fajarrahmat934@gmail.com>
2023-05-18 22:17:33 +0700

    tambah prestasi sama coba buat dropdown

--- script/script.js
+++ script/script.js
@@ -18,3 +18,17 @@ function handleMenu() {
 }
 
 handleMenu()
+
+const parentDropdown = document.querySelectorAll(
+    ".navbar-btm-menu .nav-item"
+);
+
+parentDropdown.forEach((dropdownItem, i) => {
+    console.log(dropdownItem.lastElementChild)
+    dropdownItem.addEventListener("mouseover", () => {
+        dropdownItem.lastElementChild.style.display = "flex"
+    });
+    dropdownItem.addEventListener("mouseout", () => {
+        dropdownItem.lastElementChild.style.display = "none"
+    });
+});
