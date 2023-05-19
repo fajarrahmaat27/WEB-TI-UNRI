4fa86174ece5031bc7cbc15c09dd50c368c1acf1
fajarrahmaat27 <fajarrahmat934@gmail.com>
2023-05-19 23:22:37 +0700

    dropdown sementara pada event mobile

--- script/script.js
+++ script/script.js
@@ -19,17 +19,33 @@ function handleMenu() {
 
 handleMenu()
 
-const parentDropdown = document.querySelectorAll(
-    ".navbar-btm-menu .nav-item"
-);
+// const parentDropdown = document.querySelectorAll(
+//     ".navbar-btm-menu .nav-item"
+// );
 
-parentDropdown.forEach((dropdownItem, i) => {
-    console.log(dropdownItem.lastElementChild)
-    dropdownItem.addEventListener("mouseover", () => {
-        dropdownItem.lastElementChild.style.display = "block"
-    });
-    dropdownItem.addEventListener("mouseout", () => {
-        dropdownItem.lastElementChild.style.display = "none"
-    });
-});
+// parentDropdown.forEach((dropdownItem, i) => {
+//     console.log(dropdownItem.lastElementChild)
+//     dropdownItem.addEventListener("mouseover", () => {
+//         dropdownItem.lastElementChild.style.display = "block"
+//     });
+//     dropdownItem.addEventListener("mouseout", () => {
+//         dropdownItem.lastElementChild.style.display = "none"
+//     });
+// });
+
+//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown1 = document.getElementsByClassName("dropdown1-btn");
+var i;
+
+for (i = 0; i < dropdown1.length; i++) {
+  dropdown1[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {    
+      dropdownContent.style.display = "block";
+    }
+  });
+}
 
